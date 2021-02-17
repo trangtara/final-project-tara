7af00f4a5024c154c2e1508f972b835e4fc6757c
Trang Tara <trangnusi@gmail.com>
2021-02-17 12:03:04 +0100

    change url

--- backend/emailTemplate.js
+++ backend/emailTemplate.js
@@ -1,8 +1,10 @@
 
 export const qrCodeEmailTemplate = ({ inviteeName, inviteeId}) => {
+	// const  URL = `http://localhost:3000/confirmation/${inviteeId}`
+	const URL = `https://icheckin.netlify.app/${inviteeId}`
   let emailContent = `<h2>Dear ${inviteeName}</h2><br>`;
 	emailContent += 'You are invited to the upcoming event. Please use this qrCode to checkin'
 	emailContent += `<p> Please follow the link below to confirm your participation: </p>`;
-	emailContent += `<a href="http://localhost:3000/confirmation/${inviteeId}">Confirm</a>`
+	emailContent += `<a href="${URL}">Confirm</a>`
 	return emailContent
 }
